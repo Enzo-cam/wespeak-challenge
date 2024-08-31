de646cd6594d1fce82d59c02da08085d36247762
Enzo-cam <camerac09@gmail.com>
2024-08-31 18:36:00 -0300

    Feat: cron-job by Vercel

--- src/app/api/reset-counter/route.ts
+++ src/app/api/reset-counter/route.ts
@@ -2,7 +2,13 @@ import { NextResponse } from 'next/server';
 import { prisma } from '../../../../lib/prisma';
 import { revalidatePath } from 'next/cache';
 
-export async function POST() {
+export async function POST(request: Request) {
+  
+   // Verificar la autenticación del CronSecret(es lo que recomienda vercel)
+   const authHeader = request.headers.get('authorization');
+   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+   }
   try {
     await prisma.counter.updateMany({
       data: { value: 0 },
@@ -15,4 +21,9 @@ export async function POST() {
     console.error('Fallo en reset counter:', error);
     return NextResponse.json({ error: 'Hubo un error al setear el counter' }, { status: 500 });
   }
+}
+
+// Manejar solicitudes OPTIONS para CORS si es necesario
+export async function OPTIONS(request: Request) {
+  return NextResponse.json({}, { status: 200 });
 }
